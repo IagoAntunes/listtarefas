d3675f6c16708e679f379be075cbec57b907a165
IagoAntunes <iagoantunes.f@gmail.com>
2022-09-11 20:50:49 -0300

    Slidable

--- lib/Widgets/todo_list_item.dart
+++ lib/Widgets/todo_list_item.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:intl/intl.dart';
 
 import '../models/todo.dart';
@@ -9,26 +8,40 @@ class TodoListItem extends StatelessWidget {
   const TodoListItem({super.key, required this.todo});
 
   final Todo todo;
+  void doNothing(BuildContext context) {}
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: Colors.grey[200],
-      ),
-      margin: const EdgeInsets.all(2),
-      padding: const EdgeInsets.all(16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            DateFormat('dd/MM/yyyy - HH:mm').format(todo.dateTime),
-            style: const TextStyle(fontSize: 12),
-          ),
-          Text(todo.title,
-              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w600))
-        ],
+    return Slidable(
+      startActionPane: ActionPane(motion: const ScrollMotion(), children: [
+        SlidableAction(
+            onPressed: doNothing,
+            backgroundColor: const Color(0xFFFE4A49),
+            foregroundColor: Colors.white,
+            icon: Icons.delete,
+            autoClose: true,
+            borderRadius: BorderRadius.circular(8),
+            label: 'Delete')
+      ]),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.grey[200],
+        ),
+        margin: const EdgeInsets.all(2),
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              DateFormat('dd/MM/yyyy - HH:mm').format(todo.dateTime),
+              style: const TextStyle(fontSize: 12),
+            ),
+            Text(todo.title,
+                style:
+                    const TextStyle(fontSize: 16, fontWeight: FontWeight.w600))
+          ],
+        ),
       ),
     );
   }
